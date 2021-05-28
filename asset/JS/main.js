8ca98b824608568ffb390c6d9c3805c5751aaab0
planeset94 <planeset94@gmail.com>
2021-05-28 18:32:34 +0200

    Bonus: add selector & GenrseFucntion

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -7,22 +7,50 @@ const app = new Vue(
         data: {
             url: "https://flynn.boolean.careers/exercises/api/array/music",
             posters: [],
+            genereUtente: '',
+
         },
 
 
 
         methods: {
+            genreSelection(genereUtente) {
+                this.posters.forEach(card => {
+                    if (card.genre.includes(genereUtente)) {
+                        card.visible = true
+
+                    } else {
+                        card.visible = false
+                    }
+                    console.log(card);
+
+
+                });
+
+
+
+            },
+
+
+
+
 
         },
 
 
 
         mounted() {
+
             axios
                 .get(this.url)
                 .then(response => {
                     // console.log(response.data);
+
                     this.posters = response.data.response
+                    this.posters.forEach(el => {
+                        el.visible = true
+                        // console.log(el);
+                    })
                     // console.log(this.posters);
                 }).catch(e => {
                     console.error(e);
